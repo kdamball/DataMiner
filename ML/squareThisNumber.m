ee74449f6fe2a980a5f021f2833948bc6d8f5080
Kado Damball <kdamball@users.noreply.github.com>
2015-07-16 17:34:47 +0300

    Removing silly file

--- ML/squareThisNumber.m
+++
@@ -1,8 +0,0 @@
-function J = squareThisNumber(X, y, theta)
-% X is the "design matrix" containing our training examples
-% y is the class labels
-
-m = size(X,1);					%number of training egs
-predictions = X*theta; 	%predictions of hypothesis on all m examples
-sqErrors = (predictions-y).^2; % square errors
-J = 1/(2*m) * sum(sqErrors);
